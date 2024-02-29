cf665b6ffc59671548a9d22d6003d439852d1120
mdfarrukh313 <mdfarrukh313@gmail.com>
2024-02-29 17:30:26 +0500

    js file created

---
+++ app.js
@@ -0,0 +1,92 @@
+// var num1 =  + prompt ("Enter your first number")
+// var num2 =  + prompt ("Enter your second number")
+// if (num1 === num2){
+//     alert(num1  + " " + "is equel to " + " " + num2)
+// } else if (num1 > num2) {
+//     alert(num1 + " " + "is larger than" + " " + num2)
+// } 
+//  else if (num1 < num2) {
+//     alert(num1 + " " + "is less than" + " " + num2)
+// } else (num1 , num2 === ([])) (
+//     alert("wrong answer")
+// )
+
+// document.write("<br> <br>")
+
+// var num3 = prompt ("Enter your number")
+// if (num3 > "0") {
+//     alert("Your number is positive")
+// }else if (num3 < "0"){
+//     alert("Your number is negetive")
+// }else (num3 == "0" ) (
+//     alert("Your number is zero")
+// )
+
+// document.write("<br> <br>")
+
+// var vowel = prompt ("Please enter one alphabet character in large case")
+// if (vowel == "A" ){
+//     alert("This letter is vowel")
+// } else if (vowel == "I") {
+//     alert("This letter is vowel")
+// }
+//  else if (vowel == "E") {
+//     alert("This letter is vowel")
+// }
+// else if (vowel == "O") {
+//     alert("This letter is vowel")
+// }
+// else if (vowel == "U") {
+//     alert("This letter is vowel")
+// } else alert ("This letter is consonant")
+
+// document.write("<br> <br>")
+
+// var code =prompt(" Enter your code ")
+// if (code >= 65  &&  code <=90   ){
+//       alert(" your have typed a capital letter")
+// }   else if  (code >= 97 &&  code <=122   ){
+//       alert(" your have typed a small letter")
+// } else if  (code >= 48 &&  code <=57   ){
+//       alert(" your have typed a number")
+// }
+
+// document.write("<br> <br>")
+
+// var pass = 12345;
+// var pass1 =  prompt ("Please enter password")
+// var pass2 =  prompt ("please reenter password")
+// if (pass1 === pass2 ) {
+//     alert("Correct!")
+// } else if (pass1 !== pass2 ) {
+//     alert("“Incorrect password”")
+// } 
+//  else if (pass1 , pass2 === pass) {
+//     alert("“Incorrect password”")
+// } 
+// else ("“ Please enter your password”")
+
+// document.write("<br> <br>")
+
+// var hour = prompt ("Enter hour in 24 format")
+// if (hour >= 17  && hour <= 24  ) {
+// alert ( "Good evening")
+// } else if (hour <= 16 ) {
+//     alert ("Good day") 
+// } else alert ("Enter hour in 24 format")
+
+document.write("<br> <br>")
+
+var time = prompt ("Please enter time in 24 format")
+if (time >= 0 && time <= 12) {
+    alert ("Good Morning")
+} else if (time >= 12 && time <= 17) {
+    alert ("Good afternoon")
+}
+ else if (time >= 17 && time <= 21) {
+    alert ("Good evening")
+}
+ else if (time >= 21 && time <= 24) {
+    alert ("Good night")
+} else alert ("Please enter time in 24 format")
+
